e2f9f5bcd2a9476c5aab9b8e6ca0dd60d61762eb
Bizzy-coding <bizzycoding@gmail.com>
2023-06-02 21:53:31 +0100

    refactoring project js

--- scripts.js
+++ scripts.js
@@ -66,61 +66,61 @@ const projects = [
 ];
 
 const projectCont = document.querySelector(".projects-container");
-
 projects.forEach((project) => {
-  var projectCard = document.createElement("div"),
-    projectImgWrap = document.createElement("div"),
-    projectImg = document.createElement('img'),
-    projectGif = document.createElement('img'),
-    projectTxt = document.createElement("div");
+  const projectCard = document.createElement("div");
+  const projectImgWrap = document.createElement("div");
+  const projectImg = document.createElement('img');
+  const projectGif = document.createElement('img');
+  const projectTxt = document.createElement("div");
+  
   projectCard.classList.add("project-card");
   projectImgWrap.classList.add("project-img-wrap");
   projectImg.classList.add("project-img");
   projectGif.classList.add("project-gif");
   projectTxt.classList.add("project-txt");
+  
   projectCont.appendChild(projectCard);
   projectCard.appendChild(projectImgWrap);
   projectCard.appendChild(projectTxt);
-  console.log(projectImg);
+
   projectTxt.innerHTML = `
-        <h3>${project.name}</h3>
-        <p>${project.blurb}</p>
-        <h3>Built Using</h3>
-    `;
-
-    var lilacArrow = `<svg class="lilac-arrow" width="84" height="31" viewBox="0 0 84 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M82.9258 16.7904C83.7104 16.0129 83.7162 14.7466 82.9387 13.962L70.269 1.17612C69.4915 0.391513 68.2252 0.385736 67.4406 1.16321C66.656 1.94069 66.6502 3.20701 67.4277 3.99161L78.6896 15.3568L67.3244 26.6188C66.5398 27.3963 66.534 28.6626 67.3115 29.4472C68.089 30.2318 69.3553 30.2376 70.1399 29.4601L82.9258 16.7904ZM0.472321 17L81.5089 17.3697L81.5272 13.3697L0.49057 13L0.472321 17Z" fill="#AFB5E8"/>
-  </svg>
+    <h3>${project.name}</h3>
+    <p>${project.blurb}</p>
+    <h3>Built Using</h3>
   `;
-
+  
+  const lilacArrow = `<svg class="lilac-arrow" width="84" height="31" viewBox="0 0 84 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M82.9258 16.7904C83.7104 16.0129 83.7162 14.7466 82.9387 13.962L70.269 1.17612C69.4915 0.391513 68.2252 0.385736 67.4406 1.16321C66.656 1.94069 66.6502 3.20701 67.4277 3.99161L78.6896 15.3568L67.3244 26.6188C66.5398 27.3963 66.534 28.6626 67.3115 29.4472C68.089 30.2318 69.3553 30.2376 70.1399 29.4601L82.9258 16.7904ZM0.472321 17L81.5089 17.3697L81.5272 13.3697L0.49057 13L0.472321 17Z" fill="#AFB5E8"/>
+  </svg>`;
+  
   project.techStack.forEach((tech) => {
-    var icon = document.createElement("img");
+    const icon = document.createElement("img");
     icon.classList.add('tech-icon')
     icon.src = techIcons[tech]; 
     projectTxt.appendChild(icon);
   });
-//PROJECT IMAGES 
+  
+  // PROJECT IMAGES 
   projectImg.src = project.img;
   projectGif.src = project.gif;
   projectImgWrap.appendChild(projectImg);
   projectImgWrap.appendChild(projectGif);
+  
   projectImgWrap.addEventListener('mouseenter', () => {
-  projectGif.classList.add('active');
-  })
+    projectGif.classList.add('active');
+  });
+  
   projectImgWrap.addEventListener('mouseleave', () => {
     projectGif.classList.remove('active');
-    })
-
-
-//PROJECT BTNS 
-  var projectBtn = document.createElement('a');
+  });
+  
+  // PROJECT BUTTONS
+  const projectBtn = document.createElement('a');
   projectBtn.classList.add('project-btn');
   projectBtn.href = project.url;
-  projectBtn.target = "blank";
-  projectBtn.innerHTML = `
-  view
-  `;
-
+  projectBtn.target = "_blank";
+  projectBtn.textContent = 'View';
+  
   projectTxt.appendChild(projectBtn);
-
 });
+
